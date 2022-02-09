1af535cb381ebd20795b9ca9ba44c3f301fe37e5
mrustl <michael.rustler@kompetenz-wasser.de>
2022-02-09 15:02:13 +0100

    Add :book: for dataset

---
+++ R/data_materials.R
@@ -0,0 +1,19 @@
+#' Materials
+#'
+#' A dataset containing hydraulic soil characteristics from Hydrus1D GUI database
+#'
+#' @format A data frame with 12 rows and 7 variables:
+#' \describe{
+#'   \item{material_name}{Name of soil}
+#'   \item{Qr}{Residual soil water content}
+#'   \item{Qs}{Saturated soil water content}
+#'   \item{Qr}{Parameter a in the soil water retention function (unit: L-1, here: cm)}
+#'   \item{n}{Parameter n in the soil water retention function}
+#'   \item{Ks}{Saturated hydraulic conductivity (unit: LT-1, here: cm/day)}
+#'   \item{T}{Tortuosity parameter in the conductivity function}
+#' }
+#' @source Hydrus1D GUI -> Soil Hydraulic Properties (Pre-Processing Menu, Water Flow Submenu)
+#' @examples
+#' materials
+"materials"
+
