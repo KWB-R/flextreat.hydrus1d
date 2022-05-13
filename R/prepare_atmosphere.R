ddb847f8ee725731e59315c716ae55d86d8f2057
mrustl <michael.rustler@kompetenz-wasser.de>
2022-05-13 17:49:34 +0200

    add NA replacement and fix HYDRUS run

--- R/prepare_atmosphere.R
+++ R/prepare_atmosphere.R
@@ -7,6 +7,7 @@
 #' @importFrom dplyr mutate rename select
 #' @importFrom tidyselect all_of
 #' @importFrom kwb.hydrus1d defaults_atmosphere
+#' @importFrom tidyr replace_na
 #' @examples
 #'atm <- prepare_atmosphere_data()
 #'atm_selected <- select_hydrologic_years(atm)
@@ -16,6 +17,10 @@ prepare_atmosphere <- function(atm,
 
 
 inputs <- atm %>%
+  dplyr::mutate(rain_mm = tidyr::replace_na(.data$rain_mm, 0),
+                groundwater.mmPerDay = tidyr::replace_na(.data$groundwater.mmPerDay, 0),
+                clearwater.mmPerDay = tidyr::replace_na(.data$clearwater.mmPerDay, 0),
+                evapo_p_mean_mm = tidyr::replace_na(.data$evapo_p_mean_mm, 0)) %>%
   dplyr::mutate(tAtm = dplyr::row_number(),
                 Prec = .data$rain_mm + .data$groundwater.mmPerDay + .data$clearwater.mmPerDay) %>%
   dplyr::rename(rSoil = .data$evapo_p_mean_mm) %>%
---
+++ data-raw/write_atmosphere.R
@@ -0,0 +1,42 @@
+library(flextreat.hydrus1d)
+
+atm <- prepare_atmosphere_data()
+atm_selected <- select_hydrologic_years(atm)
+atm_prep <- prepare_atmosphere(atm_selected)
+
+
+atmos <- kwb.hydrus1d::write_atmosphere(atm = atm_prep,
+                                        MaxAL = nrow(atm_prep)
+                                        )
+
+
+writeLines(text = atmos,
+           "inst/extdata/model/test/ATMOSPH.IN")
+
+
+
+writeLines(text = atmos,
+           system.file("extdata/model/test/ATMOSPH.IN", package = "flextreat.hydrus1d"))
+
+
+
+exe_dir <- "C:/Program Files (x86)/PC-Progress/Hydrus-1D 4.xx"
+exe_name <- "H1D_CALC.EXE"
+exe_path <- file.path(exe_dir, exe_name)
+
+target_dir <- "inst/extdata/model"
+target_path <- file.path(target_dir, exe_name)
+
+fs::file_copy(path = exe_path,
+              new_path = fs::path_abs(target_path)
+              )
+
+writeLines(fs::path_abs(sprintf("%s/%s\n",
+                   target_dir,
+                   "test")),
+           fs::path_abs(sprintf("%s/LEVEL_01.DIR",
+                   target_dir)))
+shell(cmd = sprintf("cd %s && %s",
+                    fs::path_abs(target_dir),
+                    exe_name),
+      intern = FALSE)
