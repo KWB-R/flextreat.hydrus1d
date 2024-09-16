f1c0585242080eda76d811eb0d3700d3c4803895
mrustl <michael.rustler@kompetenz-wasser.de>
2024-09-16 11:53:40 +0200

    update irrigation until 2023-12

--- data-raw/update_irrigation.R
+++ data-raw/update_irrigation.R
@@ -34,3 +34,43 @@ irrig_new <- dplyr::bind_rows(irrig_old, irrig_update_tmp) %>%
   dplyr::arrange(Typ, Jahr, Monat_num)
 
 readr::write_csv2(irrig_new, "inst/extdata/input-data/Beregnungsmengen_AVB.csv")
+
+irrig_old %>%
+  dplyr::rename(year = Jahr,
+                month = Monat_num,
+                days_in_month =
+                )
+
+
+irrigation <- irrig_old %>%
+  dplyr::rename(irrigation_m3 = .data$Menge_m3,
+                source = .data$Typ,
+                month = .data$Monat_num,
+                year = .data$Jahr) %>%
+  dplyr::mutate(date_start = as.Date(sprintf("%d-%02d-01",
+                                             .data$year,
+                                             .data$month)),
+                days_in_month = as.numeric(lubridate::days_in_month(.data$date_start)),
+                date_end =  as.Date(sprintf("%d-%02d-%02d",
+                                            .data$year,
+                                            .data$month,
+                                            .data$days_in_month)),
+                source = kwb.utils::multiSubstitute(.data$source,
+                                                    replacements = list("Grundwasser" = "groundwater.mmPerDay",
+                                                                        "Klarwasser" = "clearwater.mmPerDay")),
+                irrigation_cbmPerDay = .data$irrigation_m3/.data$days_in_month,
+                irrigation_area_sqm = irrigation_area_sqm,
+                irrigation_mmPerDay = 1000*irrigation_cbmPerDay/irrigation_area_sqm) %>%
+  dplyr::select(.data$year,
+                .data$month,
+                .data$days_in_month,
+                .data$date_start,
+                .data$date_end,
+                .data$source,
+                .data$irrigation_mmPerDay,
+                .data$irrigation_area_sqm) %>%
+  tidyr::pivot_wider(names_from = .data$source,
+                     values_from = .data$irrigation_mmPerDay)
+
+usethis::use_data(irrigation)
+
